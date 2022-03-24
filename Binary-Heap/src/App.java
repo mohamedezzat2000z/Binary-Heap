e243280b4084fc1b7cf4a4f6a05fa0830875c296
aaserfawzy <aaserkind537@gmail.com>
2022-03-24 16:36:06 +0200

    binarysort

--- Binary-Heap/src/App.java
+++ Binary-Heap/src/App.java
@@ -1,8 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        mergeSort s= new mergeSort();
+        binaryHeap s= new binaryHeap();
         int k[]={10,20,5,6,90,3,2,14,35};
-        s.sort(k, 0, 9-1);
+        s.sort(k);
         for(int i=0;i<9;i++){
             System.out.print(k[i] +" ");
         }
--- Binary-Heap/src/binaryHeap.java
+++ Binary-Heap/src/binaryHeap.java
@@ -2,8 +2,16 @@ public class binaryHeap {
 
     private int[] array;
 
-    public binaryHeap(int[] o) {
+  /*  public binaryHeap(int[] o) {
         this.array = o;
+    }*/
+
+    public void bulidMaxHeap(int[] nums){
+        int x=nums.length;
+        int i;
+        for(i=(x/2); i>=0 ;i--){
+            heapify(nums,i,x);
+        }
     }
 
     public void insert(Object o) {
@@ -11,15 +19,52 @@ public class binaryHeap {
     }
 
     public void delete(Object o) {
-
+        int x=array.length;
+            int s=array[0];
+            array[0]=array[x-1];
+            array[x-1]=s;
+            
+            heapify(array,0,x-1);
     }
 
     public void sort(int[] array) {
-        
+        bulidMaxHeap(array);
+        int x=array.length;
+        int i;
+        for(i=1;i<x;i++){
+            int s=array[0];
+            array[0]=array[x-i];
+            array[x-i]=s;
+            
+            heapify(array,0,x-i);
+          
+      }
+
 
     }
 
-    public void heapify() {
+    public void heapify(int[] nums, int k,int len) {
 
+        int hole =k;
+        while(hole*2+1<len){
+            int left =hole*2+1;
+            int right =left+1;
+            int target;
+            if(right<len && (nums[right]>nums[left])){
+                target=right;
+            }else{
+                target=left;
+            }
+            
+            if (nums[hole]<nums[target]){
+                int s=nums[hole];
+                nums[hole]=nums[target];
+                nums[target]=s;
+                hole=target;
+             
+            }else{
+                break;
+            }
+         }
     }
 }

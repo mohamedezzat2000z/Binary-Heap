2fd75cbfa9457b073e109406c867f51dcae17592
mohamedezzat2000z <mohamedezzat2000z@gmail.com>
2022-04-01 19:10:17 +0200

    comments

--- Binary-Heap/src/binaryHeap.java
+++ Binary-Heap/src/binaryHeap.java
@@ -21,9 +21,9 @@ public class binaryHeap {
      */
     public void bulidMaxHeap(int[] nums) {
         int x = nums.length;// get the length of the array
-        int i;// sitting a varabile 
+        int i;// sitting a varabile
         for (i = (x / 2); i >= 0; i--) {// loop over the upper part have of the array from the lowest element
-            heapify(nums, i, x);// allocate downward
+            heapify(nums, i, x);// allocate downwards
         }
     }
 
@@ -42,34 +42,34 @@ public class binaryHeap {
         }
     }
 
-    
     /**
      * delete an element from the heap
      * 
      */
     public void delete() {
-        if(size>0){// check if the array has element
-        array[0] = array[size - 1];// replace the max elemtn with the smallest
-        size--;// decemint the cunter
-        heapify(array, 0, size);//alocate the eleemtn dowenward
-        }else{
+        if (size > 0) {// check if the array has element
+            array[0] = array[size - 1];// replace the max element with the smallest
+            size--;// decerement the counter
+            heapify(array, 0, size);// alocate the eleemtn downward
+        } else {
             System.out.println("array is empty");
         }
     }
 
     /**
      * sort the given array using heap sort
+     * 
      * @param array array to be sorted
      */
     public void sort(int[] array) {
         bulidMaxHeap(array);// bulid max heap
         int x = array.length;// get the the length of the array
         int i;// set the varablies
-        for (i = 1; i < x; i++) {// loop over the etnterd array
-            int s = array[0];//get the max element
-            array[0] = array[x - i];//replace the first elemetn to the first element
-            array[x - i] = s;//but the max element in the last of the array
-            heapify(array, 0, x - i);//allocate the elemt down 
+        for (i = 1; i < x; i++) {// loop over the entered array
+            int s = array[0];// get the max elements
+            array[0] = array[x - i];// replace the first element to the first element
+            array[x - i] = s;// but the max element in the last of the array
+            heapify(array, 0, x - i);// allocate the element down
 
         }
 
@@ -93,35 +93,36 @@ public class binaryHeap {
         // if not do nothing
     }
 
-
     /**
-     * heapify with preallocate up
+     * heapify with preallocate down
+     * 
      * @param nums the array where the heapify will happens
-     * @param s current size of the heap
-     * @param k element after insertion
+     * @param s    current size of the heap
+     * @param k    element after insertion
      */
     public void heapify(int[] nums, int k, int len) {
 
-        int hole = k;// get the current place of the element to allocate downfrom this place
-        while (hole * 2 + 1 < len) {// check if the node didnt6 reach the root
-            int left = hole * 2 + 1;//get the left element
-            int right = left + 1;//get the right element
-            int target;//set the varabklie for the next target for the element ot go to
-            if (right < len && (nums[right] > nums[left])) {// check if the right exist and is larget tha the left next element
+        int hole = k;// get the current place of the element to allocate down from this place
+        while (hole * 2 + 1 < len) {// check if the node did not reach the root
+            int left = hole * 2 + 1;// get the left element
+            int right = left + 1;// get the right element
+            int target;// set the variable for the next target for the element ot go to
+            if (right < len && (nums[right] > nums[left])) {// check if the right exist and is larget than the left next
+                                                            // element
                 target = right;// the next target right
             } else {
-                target = left;// the next targetleft
+                target = left;// the next target left
             }
 
-            if (nums[hole] < nums[target]) {// if the target elemt is biggeer than the 
-                //swaping steps
+            if (nums[hole] < nums[target]) {// if the target element is bigger than the hole
+                // swaping steps
                 int s = nums[hole];
                 nums[hole] = nums[target];
                 nums[target] = s;
-                hole = target;//update the postion of the hole
+                hole = target;// update the postion of the hole
 
             } else {
-                break;// break if the element is larger then its childs
+                break;// break if the element is larger then its children
             }
         }
     }
